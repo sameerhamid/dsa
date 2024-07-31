039b37fa607e66d0dcdcb421ce466ffff6173f65
SAMEER <93476000+sameerhamid@users.noreply.github.com>
2024-07-31 04:56:39 +0000

    print 5555 33333 11111 in sequence

---
+++ sixth.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  
+  for(int i=5;i>=1;i=i-2){
+    for(int j=1;j<=5;j++){
+      cout << i<<" ";
+    }
+    cout << "\n";
+  }
+    
+    return 0;
+}

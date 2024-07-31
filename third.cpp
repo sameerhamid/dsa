57b56cd4fea8dd817c6f896f6cdcda09f9f1340a
SAMEER <93476000+sameerhamid@users.noreply.github.com>
2024-07-31 04:51:14 +0000

    print 1234 in sequence

---
+++ third.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  
+  for(int i=3;i>=1;i--){
+    for(int j=1;j<=5;j++){
+      cout << j<<" ";
+    }
+    cout << "\n";
+  }
+    
+    return 0;
+}

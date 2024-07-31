36d6506086752dab94f8ae64745afc516d5e698d
SAMEER <93476000+sameerhamid@users.noreply.github.com>
2024-07-31 04:52:16 +0000

    print 54321 in sequence

---
+++ fourth.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  
+  for(int i=3;i>=1;i--){
+    for(int j=5;j>0;j--){
+      cout << j<<" ";
+    }
+    cout << "\n";
+  }
+    
+    return 0;
+}

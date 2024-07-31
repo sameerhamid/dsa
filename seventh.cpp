07914b8535b2d75d1fd046053634c89ad2a5ce74
SAMEER <93476000+sameerhamid@users.noreply.github.com>
2024-07-31 04:58:48 +0000

    print 1 3 5 7 9 in sequence

---
+++ seventh.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  
+  for(int i=1;i<=3;i++){
+    for(int j=1;j<=9;j=j+2){
+      cout << j<<" ";
+    }
+    cout << "\n";
+  }
+    
+    return 0;
+}

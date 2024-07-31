ce7acd924709e2cbe60ab7af510f5d3b74f41ef4
SAMEER <93476000+sameerhamid@users.noreply.github.com>
2024-07-31 04:44:14 +0000

    add first programme

---
+++ first.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  
+  for(int i=1;i<=3;i++){
+    for(int j=0;j<5;j++){
+      cout << i<<" ";
+    }
+    cout << "\n";
+  }
+    
+    return 0;
+}
